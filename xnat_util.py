26c0bd6311a76920d2bfa0c5333599834a3f9ea2
Scott Burns <scott.s.burns@gmail.com>
2011-09-19 15:01:22 -0500

    Serious refactor of xnat utility functions

--- xnat_util.py
+++ xnat_util.py
@@ -2,8 +2,45 @@ import os
 from ConfigParser import ConfigParser
 
 from pyxnat import Interface
+from pyxnat.core.resources import Project, Subject, Experiment, Scan
+from pyxnat.core.errors import DatabaseError
 
-def load_xnat(cfg=os.path.join(os.path.expanduser('~'), '.xnat.cfg')):
+ALLOWED_KEYS = {Subject: set(['group', 'src', 'pi_lastname',
+                    'pi_firstname','dob', 'yob', 'age', 'gender',
+                    'handedness', 'ses', 'education', 'educationDesc',
+                    'race', 'ethnicity', 'weight', 'height',
+                    'gestational_age','post_menstrual_age',
+                    'birth_weight']),
+                Project: set(['ID', 'secondary_ID', 'name',
+                    'description', 'keywords', 'alias', 'pi_lastname',
+                    'pi_firstname', 'note']),
+                Experiment: set(['visit_id', 'date', 'ID', 'project',
+                    'label', 'time', 'note', 'pi_firstname', 'pi_lastname',
+                    'validation_method', 'validation_status',
+                    'validation_date', 'validation_notes', 'subject_ID',
+                    'subject_label', 'subject_project', 'scanner',
+                    'operator', 'dcmAccessionNumber', 'dcmPatientId',
+                    'dcmPatientName', 'session_type', 'modality',
+                    'UID', 'coil', 'fieldStrength', 'marker',
+                    'stabilization', 'studyType', 'patientID',
+                    'patientName', 'stabilization', 'scan_start_time',
+                    'injection_start_time', 'tracer_name',
+                    'tracer_startTime', 'tracer_dose', 'tracer_sa',
+                    'tracer_totalmass', 'tracer_intermediate',
+                    'tracer_isotope', 'tracer_transmissions',
+                    'tracer_transmissions_start']),
+                Scan: set(['ID', 'type', 'UID', 'note', 'quality',
+                    'condition', 'series_description', 'documentation',
+                    'scanner', 'modality', 'frames', 'validation_method',
+                    'validation_status', 'validation_date',
+                    'validation_notes', 'coil', 'fieldStrength', 'marker',
+                    'stabilization', 'orientation', 'scanTime',
+                    'originalFileName', 'fileType', 'transaxialFOV',
+                    'acqType', 'facility', 'numPlanes', 'numFrames',
+                    'numGates', 'planeSeparation', 'binSize', 'dataType'])}
+
+
+def xnat(cfg=os.path.join(os.path.expanduser('~'), '.xnat.cfg')):
     """Initialize and test xnat connection from a previously-stored cfg file
 
     Parameters
@@ -43,16 +80,17 @@ def load_xnat(cfg=os.path.join(os.path.expanduser('~'), '.xnat.cfg')):
     # in the list of users. Any errors are passed to the caller.
     if user not in xnat.manage.users():
         raise ValueError('This XNAT is weird.')
+    xnat._memtimeout = 0.001
     return xnat
 
-def project(xnat, proj_name, proj_data={}):
+def project(xnat, name, proj_data={}):
     """ Create a new project/update project info
     
     Parameters
     ----------
     xnat: pyxnat.Interface object
         The connection to your xnat system
-    proj_name: str
+    name: str
         Project name
     proj_data: dict
         Project data you'd like to initialize 
@@ -61,18 +99,14 @@ def project(xnat, proj_name, proj_data={}):
     pjt: Project object
         
     """
-    pjt = xnat.select.project(proj_name)
-    if not pjt.exists():
-        pjt.create()
+    pjt = _check_parent_and_get(xnat, xnat.select.project, name)
     if proj_data:
-        passed_check, bad_keys = _key_check('project', proj_data.keys())
-        if passed_check:
-            pjt.attrs.mset(proj_data)
-        else:
-            raise ValueError("Bad project keys: %s" % ' '.join(bad_keys))
+        succeeded, bad = _update_metadata(pjt, proj_data)
+        if not succeeded:
+            raise ValueError("Bad project keys: %s" % ' '.join(bad))
     return pjt
 
-def subject(project, name, demo={}):
+def subject(project, name, sub_data={}):
     """ Create a new subject/Update subject info
     
     WARNING: Previously stored data will be overwritten
@@ -83,7 +117,7 @@ def subject(project, name, demo={}):
         An established project
     name: str
         Subject identifier
-    demo: dict
+    data: dict
         Demographic data to set in xnat
         See 'xnat:subjectData' at
         http://docs.xnat.org/XNAT+REST+XML+Path+Shortcuts
@@ -93,28 +127,165 @@ def subject(project, name, demo={}):
     -------
     sub: a valid subject object
     """
-    if not project.exists():
-        raise ValueError("Project %s doesn't exist" % project.id() )
-    sub = project.subject(name)
-    if not sub.exists():
-        sub.create()
-    #  multiple attribute set
-    if demo:
-        #  let NotImplementedError keep going
-        passed_check, bad_keys = _key_check('subject', demo.keys())
-        if passed_check:
-            sub.attrs.mset(demo)
-        else:
-            raise ValueError("Bad demographics keys: %s" % ' '.join(bad_keys))
+    sub = _check_parent_and_get(project, project.subject, name)
+    if sub_data:
+        succeeded, bad = _update_metadata(sub, sub_data)
+        if not succeeded:
+            raise ValueError("Bad subject data keys: %s" % ' '.join(bad))
     return sub
+
+def experiment(subject, name, exp_data={}):
+    """ Create/Update a subject's experiment
+    
+    Parameters
+    ----------
+    subject: Subject object
+        The subject to which you want to create/update the experiment
+    name: str
+        Experiment string name
+    exp_data: dict
+        Experiment metadata to set in xnat
+        See 'xnat:experimentData' at
+        http://docs.xnat.org/XNAT+REST+XML+Path+Shortcuts
+        for allowed keys
+        
+    Returns
+    -------
+    exp: a valid experiment object
+    """
+    exp = _check_parent_and_get(subject, subject.experiment, name)
+    if exp_data:
+        succeeded, bad = _update_metadata(exp, exp_data)
+        if not succeeded:
+            raise ValueError("Bad experiment data keys: %s" % ' '.join(bad))
+    return exp
+        
+def scan(experiment, name, scan_data={}):
+    """ Create/Update an experiment's scan
+    
+    Parameters
+    ----------
+    experiment: experiment object
+        The experiment for which you want to create/update a scan
+    name: str
+        Scan string name
+    scan_data: dict
+        Scan metadata to set in xnat
+        See 'xnat:imageScanData' at
+        http://docs.xnat.org/XNAT+REST+XML+Path+Shortcuts
+        for allowed keys
+        
+    Returns
+    -------
+    scan: a valid scan object
+    """
+    scan = _check_parent_and_get(experiment, experiment.scan, name)
+    if scan_data:
+        succeeded, bad = _update_metadata(scan, scan_data)
+        if not succeeded:
+            raise ValueError("Bad scan data keys: %s" % ' '.join(bad))
+    return scan
+
+def resource(scan, name):
+    """ Create/Update a scan's resource
+    
+    Parameters
+    ----------
+    scan: scan object
+        The scan for which you want to create/update a resource
+    name: str
+        Resource name
+        
+    Returns
+    -------
+    res: a valid resource object
+    """
+    res = _check_parent_and_get(scan, scan.resource, name)
+    #  Not sure what to specify as far as resource metadata?
+    return res
+    
+def add_file(resource, name, fpath):
+    """ Put a file in this resource with given name
+    
+    Parameters
+    ----------
+    resource: resource object
+        The Resource under which the file should go
+    name: str
+        The name of the file
+    fpath: str
+        Path on local machine of file 
+    """
+    resource.file(name).put(fpath)
+
+def _update_metadata(xnat_obj, new_data={}):
+    """ Update metadata for a xnat object
+
+    Parameters
+    ----------
+    xnat_obj: any xnat object
+        The object whose metadata you wish to update
+    new_data: dict
+        Data whose keys must match allowed keys
+
+    Returns
+    -------
+    succeeded: bool
+        That update worked
+    bad_keys: seq
+        If succeeded, empty, otherwise a sequence of the keys not accepted by
+        xnat
+    """
+    if not xnat_obj.exists():
+        raise ValueError("This object doesn't exist in xnat")
+    succeeded, bad_keys = _key_check(type(xnat_obj), new_data.keys())
+    if succeeded:
+        #  do the mset
+        xnat_obj.attrs.mset(new_data)
+        #  TODO: check that the mset worked
+        for key, val in new_data.items():
+            good_update = xnat_obj.attrs.get(key) == val
+            if not good_update:
+                #  TODO change to warning
+                print("WARNING: %s wasn't updated" % key)
+    return succeeded, bad_keys
+        
+def _check_parent_and_get(parent, creator_fn, name):
+    """ Private method to check resource owner validity and create/return
+    the new child 
+
+    Parameters
+    ----------
+    parent: Some xnat object
+        The owning object, i.e. the object directly owned the desired child
+    creator_fn: fn
+        function handle used to create new child
+    name: str
+        label for new child
+
+    Returns
+    -------
+    child: valid xnat object guaranteed to exist in the xnat system
+    """
+    if hasattr(parent, 'exists'):
+        if not parent.exists():
+            raise ValueError("Parent %s doesn't exist" % parent)
+    child = creator_fn(name)
+    if not child.exists():
+        child.create()
+    #  This might fail, actually
+    if not child.exists():
+        msg = "Cannot create object (probably a privilege issue)"
+        raise DatabaseError(msg)
+    return child
 
 def _key_check(check_type, keys):
     """ Private method to validate parameters before resource creation.
 
     Parameters
     ----------
-    check_type: subject
-        resource type
+    check_type: type
+        resource type (call with something like (type(obj) )
     keys: iterable
         parameters to check
     Returns
@@ -124,23 +295,14 @@ def _key_check(check_type, keys):
     bad_keys: iterable
         keys the caller specified that xnat won't accept
     """
-    allowed_keys = {'subject': set(['group', 'src', 'pi_lastname',
-                        'pi_firstname','dob',' yob', 'age', 'gender',
-                        'handedness', 'ses', 'education', 'educationDesc',
-                        'race', 'ethnicity', 'weight', 'height',
-                        'gestational_age','post_menstrual_age',
-                        'birth_weight']),
-                    'project': set(['ID', 'secondary_ID', 'name',
-                        'description', 'keywords', 'alias', 'pi_lastname',
-                        'pi_firstname', 'note'])}
-    if check_type not in allowed_keys:
+    if check_type not in ALLOWED_KEYS:
         raise NotImplementedError("Cannot currently check %s" % check_type)
     key_set = set(keys)
     passed = False
     bad_keys = []
-    if allowed_keys[check_type].issuperset(key_set):
+    if ALLOWED_KEYS[check_type].issuperset(key_set):
         passed = True
     else:
-        bad_keys.extend(key_set.difference(allowed_keys[check_type]))
+        bad_keys.extend(key_set.difference(ALLOWED_KEYS[check_type]))
     return passed, bad_keys
 
