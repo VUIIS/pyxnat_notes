4142ffe9dc88e06c39d85577f49b5c61a0437292
Scott Burns <scott.s.burns@gmail.com>
2011-09-28 16:58:15 -0500

    More work on XNAT class, update architecture with some notes from recent emails

--- xnat/core.py
+++ xnat/core.py
@@ -1,86 +1,142 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import .util
+import os
+
+import util
+
 
 class XNAT(object):
     """ This is a simple wrapper for many of the above methods """
-    
-    def __init__(self, interface):
-        """ Constructor 
+
+    def __init__(self, interface=None, cfg=None):
+        """ Constructor
 
         Parameters
         ----------
         interface: pyxnat.interface
             The interface object to you xnat system
         """
-        self.xnat = interface
-        
-        self.projs = xnat.select.projects().get()
+        if interface:
+            self.xnat = interface
+        else:
+            if not cfg:
+                good_cfg = os.path.join(os.path.expanduser('~'), '.xnat.cfg')
+            else:
+                good_cfg = cfg
+            self.xnat = util.xnat(good_cfg)
+
+        self.projs = self.xnat.select.projects().get()
         self.proj = None
-        
+
         self.subs = None
         self.sub = None
-        
+
+        self.all_exps = None
         self.exps = None
         self.exp = None
-        
+
         self.scans = None
         self.scan = None
-        
+
         self.ress = None
         self.res = None
-        
+
         self.files = None
         self.file = None
-    
-    def _set_current(field, label, group, lower_fn, lower_field):
-        """ Private
-        
-        Set a given attribute to a new value
-        
-        Parameters
-        ----------
-        field: attribute
-            An attribute of self
-        label: str
-            To-be-current label for field
-        group: iter
-            Group in which label must exist
-        """
-        within = label in group
-        if not within:
-            raise ValueError("%s doesn't exist, try creating it first" % label)
-        else:
-            lower_field = None
-            field = label
-            lower_field = lower_fn().get()
-    
-    def set_project(self, proj_label):
+
+    def set_project(self, project_label):
         """ Set the current project
-        
+
         Parameters
         ----------
-        proj_label: str
+        project_label: str
             Project name
         """
-        if proj_label not in self.projs:
+        if project_label not in self.projs:
             msg = "Cannot set %s as current project, it doesn't exist"
-            print(msg % proj_label)
+            raise ValueError(msg % project_label)
         else:
-            self.proj = xnat.select.project(proj_label)
-            sub_ids = self.proj.subjects().get()
+            self.proj = self.xnat.select.project(project_label)
+
+            # Reset subs and get new subjects from project
             self.subs = None
-            self.subs = [self.proj.subject(id).label() for id in sub_ids]
-            
-            #  Reset everything below
+            self.subs = self.proj.subjects().get('label')
             self.sub = None
-            self.exps = None
-            self.exp = None            
-            self.scans = None
-            self.scan = None
-            self.ress = None
-            self.res = None
-            self.files = None
-            self.file = None
-            
+
+            #  Reset all children
+            ancestors = (self.exps, self.exp, self.scans, self.scan, self.ress,
+                            self.res, self.files, self.file)
+            for anc in ancestors:
+                anc = None
+
+    def set_subject(self, subject_label):
+        """ Set the current subject
+
+        Parameters
+        ----------
+        subject_label: str
+            subject name
+        """
+        if subject_label not in self.subs:
+            msg = "Cannot set %s as current subject, it doesn't exist"
+            raise ValueError(msg % subject_label)
+        self.sub = self.proj.subject(subject_label)
+
+        # Reset exps and get new experiments from subject
+        self.exps = None
+        self.exps = self.sub.experiments().get('label')
+        self.exp = None
+
+        #  Reset all children
+        ancestors = (self.scans, self.scan, self.ress, self.res,
+                        self.files, self.file)
+        for anc in ancestors:
+            anc = None
+
+    def set_experiment(self, exp_label):
+        """ Set the current experiment
+
+        Parameters
+        ----------
+        exp_label: str
+            experiment label
+        """
+        if exp_label not in self.exps:
+            msg = "Cannot set %s as current experiment, doesn't exist"
+            raise ValueError(msg % exp_label)
+        self.exp = self.sub.experiment(exp_label)
+
+        # Reset scans, get new ones from experiment
+        self.scans = None
+        self.scans = self.exp.scans().get('label')
+        self.scan = None
+
+        #  Reset all children
+        ancestors = (self.ress, self.res, self.files, self.file)
+        for anc in ancestors:
+            anc = None
+        
+    def set_scan(self, scan_label):
+        """ Set the current scan
+
+        Parameters
+        ----------
+        scan_label: str
+            scan label
+        """
+        if exp_label not in self.exps:
+            msg = "Cannot set %s as current scan, doesn't exist"
+            raise ValueError(msg % exp_label)
+        self.exp = self.sub.scan(exp_label)
+
+        # Reset resources, get new ones from scan
+        self.ress = None
+        self.ress = self.exp.resources().get('label')
+        self.res = None
+
+        #  Reset all children
+        ancestors = (self.files, self.file)
+        for anc in ancestors:
+            anc = None
+
